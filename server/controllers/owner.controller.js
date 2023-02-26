42d7653c36432868577297953f334070e062bd16
Noelia Alejandra Intriago Sánchez <noinsanc@espol.edu.ec>
2023-02-26 15:06:21 -0500

    Owner CRUD

--- server/controllers/owner.controller.js
+++ server/controllers/owner.controller.js
@@ -2,39 +2,115 @@ var db = require('../models');
 var Owner = db.owner;
 var Op = db.Sequelize.Op;
 
-exports.create = (req, res) => {
-    console.log(req.body);
+// Crear dueño
+exports.create = async (req, res) => {
     if (!req.body.name || !req.body.lastName || !req.body.cellphone || !req.body.email || !req.body.password) {
         res.status(400).send({
-            message: "Content can not be empty! "
+            message: "Content can not be empty!"
         });
-        return;
     }
     var owner = { ...req.body };
-    Owner.create(owner)
-    .then(data => {
+    try {
+        var data = await Owner.create(owner);
         res.send(data);
-    })
-    .catch(err => {
+    } catch(err) {
         res.status(500).send({
             message:
-                err.message || "Some error occurred while creating the Owner."
+                err.message || "Some error occurred while creating the owner."
         });
-    })
+    }
 };
 
-exports.findAll = (req, res) => {
-    Owner.findAll({
-
-    })
-    .then(data => {
-
-        res.send("hello worlds");
-    })
-    .catch(err => {
+// Obtener todos los dueños
+exports.findAll = async (req, res) => {
+    try {
+        var data = await Owner.findAll({ });
+        res.send(data);
+    } catch(err) {
         res.status(500).send({
             message:
-                err.message || "Some error occurred while creating the Owner."
+                err.message || "Some error occurred while getting all owners."
         });
-    })
+    }
+};
+
+// Obtener dueño por ID
+exports.find = async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send({
+            message: "ID not valid"
+        });
+    }
+    var id = req.params.id;
+    try {
+        var data = await Owner.findByPk(id);
+        res.send(data);
+    } catch(err) {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while getting the owner."
+        });
+    }
+};
+
+// Actualizar dueño
+exports.update = async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send({
+            message: "ID not valid"
+        });
+    } else if (!req.body.name || !req.body.lastName || !req.body.cellphone || !req.body.email || !req.body.password) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+    }
+    var id = req.params.id;
+    try {
+        var data = await Owner.update(req.body, {
+            where: {id: id}
+        });
+        if (data == 1) {
+            res.send({
+                message: "Owner was updated successfully"
+            });
+        } else {
+            res.send({
+                message: "Cannot update Owner with id=${identification}. Maybe Owner was not found or req.body is empty"
+            });
+        }
+    } catch(err) {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while updating the owner."
+        });
+    }
+};
+
+// Eliminar dueño
+exports.delete = async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).send({
+            message: "ID not valid"
+        });
+    }
+    var id = req.params.id;
+    try {
+        var data = await Owner.destroy({
+            where: {id: id}
+        });
+        if (data == 1) {
+            res.send({
+                message: "Owner was deleted successfully"
+            });
+        } else {
+            res.send({
+                message: "Cannot delete Owner with id=${identification}. Maybe Owner was not found or req.body is empty"
+            });
+        }
+    } catch(err) {
+        res.status(500).send({
+            message:
+                err.message || "Some error occurred while deleting the owner."
+        });
+    }
 };
--- server/routes/owner.route.js
+++ server/routes/owner.route.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 var ownerController = require('../controllers/owner.controller');
 
-router.get('/', ownerController.findAll);
 router.post('/', ownerController.create);
+router.get('/', ownerController.findAll);
+router.get('/:id', ownerController.find);
+router.put('/:id', ownerController.update);
+router.delete('/:id', ownerController.delete);
 
 module.exports = router;
