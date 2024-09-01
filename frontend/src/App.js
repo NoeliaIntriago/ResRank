5fb8ee1b195ba2c1e9ae96f84b809fac7003d2c9
NoeliaIntriago <noinsanc@espol.edu.ec>
2024-09-01 18:12:29 -0500

    chore: Update navbar styles and add responsive adjustments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,19 +4,28 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Restaurant from "./pages/Restaurant";
 import "./App.css";
-
 import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <nav>
-          <Link to="/">Login</Link>
-          <Link to="/register">Registro</Link>
-          <Link to="/home">Home</Link>
-          <Link to="/restaurant">Restaurant</Link>
+        {/* Navbar */}
+        <nav className="navbar">
+          <div className="nav-container">
+            <Link to="/" className="nav-logo">
+              ResRank
+            </Link>
+            <div className="nav-links">
+              <Link to="/">Login</Link>
+              <Link to="/register">Registro</Link>
+              <Link to="/home">Home</Link>
+              <Link to="/restaurant">Restaurant</Link>
+            </div>
+          </div>
         </nav>
+
+        {/* Routes */}
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
