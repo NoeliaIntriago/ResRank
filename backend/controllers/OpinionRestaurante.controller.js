91179a57350a23d39c9ffc5e4b332727077c05bf
NoeliaIntriago <noinsan@espol.edu.ec>
2025-04-20 09:10:18 -0500

    feat(#10): enhance getReviews to support comentario filtering

--- backend/controllers/OpinionRestaurante.controller.js
+++ backend/controllers/OpinionRestaurante.controller.js
@@ -4,7 +4,14 @@ const { getPagingData, getPagination } = require("./utils/pagination");
 const logger = require("../services/logger");
 
 exports.getReviews = async (req, res) => {
-  const { id_bar, id_usuario, calificacion, page = 1, perPage } = req.query;
+  const {
+    id_bar,
+    id_usuario,
+    calificacion,
+    comentario,
+    page = 1,
+    perPage,
+  } = req.query;
   const { limit, offset } = getPagination(page, perPage);
 
   try {
@@ -12,6 +19,11 @@ exports.getReviews = async (req, res) => {
     if (id_bar) condition.id_bar = id_bar;
     if (id_usuario) condition.id_usuario = id_usuario;
     if (calificacion) condition.calificacion = calificacion;
+    if (comentario) {
+      condition.comentario = {
+        [Op.like]: `%${comentario}%`,
+      };
+    }
 
     const data = await OpinionRestaurante.findAndCountAll({
       where: condition,
